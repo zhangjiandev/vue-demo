966f1d4388e20107c8f3648190500141724bcbe4
zhangjian <zhangjian.developer@gmail.com>
2024-11-17 22:38:53 +0800

    vue-demo

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 8080,//端口号
+    host: true,//ip地址 或 '0.0.0.0' 或 "loaclhost"
+    open: false, //启动后是否自动打开浏览器
+    https: false, // 是否开启 https
+  },
 })
